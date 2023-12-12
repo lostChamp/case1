0b718bb3d5f7ebe9335b202fad9dd9e281f6e993
Kostya <lost.champ2007@gmail.com>
2023-12-12 20:39:51 +0500

    Set bitrix request

--- postBitrixTelegram.php
+++ postBitrixTelegram.php
@@ -7,28 +7,38 @@ $templates = [
     "question_4" => "Лечение во сне ",
     "question_5" => "Оплата ",
     "question_6" => "Куда отправить расчет ",
-    "id" => "phone"
+    "id" => "phone",
+    "phone" => "phone"
 ];
 
 //$bitrix = "https://api.tech-ud72.ru/sendBx/index.php";
-$telegram = "https://api.tech-ud72.ru/sendTg/index.php";
-
-$tempPost = $_POST;
-
+//$telegram = "https://api.tech-ud72.ru/sendTg/index.php";
+//
+//$tempPost = $_POST;
+//
 //$bitrixQuery = json_decode(file_get_contents("queryBitrix.json"), true);
-//$fieldsBitrixQuery = ["title", "utm", "utm_medium", "utm_campaign", "utm_content", "utm_term", "city"];
+//$fieldsBitrixQuery = ["utm", "utm_medium", "utm_campaign", "utm_content", "utm_term"];
 //foreach($fieldsBitrixQuery as $field) {
-//    if(isset($_POST[$field])) {
+//    if(isset($tempPost[$field])) {
 //        $bitrixQuery["options"][$field] = $tempPost[$field];
 //        unset($tempPost[$field]);
 //    }
 //}
-//$bitrixQuery["options"]["domen"] = $_SERVER["SERVER_NAME"] ?? "";
-//$bitrixQuery["options"]["data"] = $tempPost;
+//$fieldsNotRequiredBitrix = ["title", "city"];
+//foreach($fieldsNotRequiredBitrix as $field) {
+//    unset($tempPost[$field]);
+//}
+//
+//$index = 0;
+//foreach ($tempPost as $key => $value) {
+//    if($key !== "tags" && $key !== "subject" && $key !== "form_type") {
+//        $bitrixQuery["options"]["data"][$index]["name"] = $templates[$key];
+//    }
+//    $bitrixQuery["options"]["data"][$index]["value"] = is_array($value) ? implode(", ", $value) : $value;
+//    $index++;
+//}
 //$bitrixPost = json_encode($bitrixQuery, true);
 //var_dump($bitrixPost);
-
-//$ch = curl_init();
 //
 //$curl = curl_init();
 //
@@ -46,40 +56,39 @@ $tempPost = $_POST;
 //        'Content-Type: application/json'
 //    ),
 //));
-////
-////curl_close($curl);
-////
+//
+//$response = curl_exec($curl);
+//
+//curl_close($curl);
 
-$telegramQuery = json_decode(file_get_contents("queryTelegram.json"), true);
-$fieldsTelegramQuery = ["utm", "utm_medium", "city"];
-$fieldsNotRequiredTg = ["utm_campaign", "utm_content", "utm_term", "title", "city"];
-foreach($fieldsTelegramQuery as $field) {
-    if(isset($_POST[$field])) {
-        if($field === "utm_medium") {
-            $telegramQuery["options"]["medium"] = $_POST[$field];
-            unset($_POST[$field]);
-        }else {
-            $telegramQuery["options"][$field] = $_POST[$field];
-            unset($_POST[$field]);
-        }
-    }
-}
-foreach($fieldsNotRequiredTg as $field) {
-    unset($_POST[$field]);
-}
-$telegramQuery["options"]["title"] = "Заявка с сайта https://ekb.stomadent66.ru";
-$telegramQuery["options"]["domen"] = "ekb.stomatdent66.ru";
-$index = 0;
-foreach ($_POST as $key => $value) {
-    if($key !== "tags" && $key !== "subject" && $key !== "form_type") {
-        $telegramQuery["options"]["data"][$index]["name"] = $templates[$key];
-    }
-    $telegramQuery["options"]["data"][$index]["value"] = is_array($value) ? implode(", ", $value) : $value;
-    $index++;
-}
 
-$telegramPost = json_encode($telegramQuery, true);
-var_dump($telegramPost);
+//$telegramQuery = json_decode(file_get_contents("queryTelegram.json"), true);
+//$fieldsTelegramQuery = ["utm", "utm_medium", "city"];
+//$fieldsNotRequiredTg = ["utm_campaign", "utm_content", "utm_term", "title", "city"];
+//foreach($fieldsTelegramQuery as $field) {
+//    if(isset($_POST[$field])) {
+//        if($field === "utm_medium") {
+//            $telegramQuery["options"]["medium"] = $_POST[$field];
+//            unset($_POST[$field]);
+//        }else {
+//            $telegramQuery["options"][$field] = $_POST[$field];
+//            unset($_POST[$field]);
+//        }
+//    }
+//}
+//foreach($fieldsNotRequiredTg as $field) {
+//    unset($_POST[$field]);
+//}
+//$index2 = 0;
+//foreach ($_POST as $key => $value) {
+//    if($key !== "tags" && $key !== "subject" && $key !== "form_type") {
+//        $telegramQuery["options"]["data"][$index2]["name"] = $templates[$key];
+//    }
+//    $telegramQuery["options"]["data"][$index2]["value"] = is_array($value) ? implode(", ", $value) : $value;
+//    $index2++;
+//}
+//
+//$telegramPost = json_encode($telegramQuery, true);
 
 //$curl = curl_init();
 //
